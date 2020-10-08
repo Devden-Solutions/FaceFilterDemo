336fb6043abd0b46b885236b91b7b656f1ca1c55
Sabarish Kumar Murugadass <sabarish@devdensolutions.com>
2020-10-08 16:01:27 +0530

    Changed the scale of the earrings 6

--- demos/threejs/EarringsViewer/demo_gltf.js
+++ demos/threejs/EarringsViewer/demo_gltf.js
@@ -5,7 +5,7 @@ const SETTINGS = {
   gltfModelURL: 'https://devden-solutions.github.io/FaceFilterDemo/demos/threejs/EarringsViewer/DreamCatcher/glTF/earrings2.glb',
   cubeMapURL: 'Bridge2/',
   offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
-  scale: 2.2
+  scale: 1.6
 };
 
 let THREECAMERA = null;

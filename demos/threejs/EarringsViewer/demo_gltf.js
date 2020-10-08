937115c63b2684f4402a79abbcc07ac9c4386abd
Sabarish Kumar Murugadass <sabarish@devdensolutions.com>
2020-10-08 14:05:34 +0530

    Earrings Viewer 9

--- demos/threejs/EarringsViewer/demo_gltf.js
+++ demos/threejs/EarringsViewer/demo_gltf.js
@@ -5,7 +5,7 @@ const SETTINGS = {
   gltfModelURL: 'https://devden-solutions.github.io/FaceFilterDemo/demos/threejs/EarringsViewer/DreamCatcher/glTF/out.gltf',
   cubeMapURL: 'Bridge2/',
   offsetYZ: [0.3, 0], // offset of the model in 3D along vertical and depth axis
-  scale: 2.5
+  scale: 0.05
 };
 
 let THREECAMERA = null;
